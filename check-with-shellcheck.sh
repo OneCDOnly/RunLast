acc865559b25b3afd2030cc0d8a33b97a97a7d78
OneCDOnly <one.cd.only@gmail.com>
2020-10-29 07:00:58 +1000

    add support for SysV-style scripts in [init.d] directory - calls everything in [init.d] with 'start' on startup and 'stop' before shutdown, - related to https://github.com/OneCDOnly/RunLast/issues/4 - Thank you @mickroll for this great suggestion!

---
+++ check-with-shellcheck.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo -n "checking ... "
+
+shellcheck --shell=bash --exclude=1003,1090,1117,2016,2021,2086,2155,2181,2206,2207 ./shared/runlast.sh && echo 'passed!' || echo 'failed!'
--- shared/runlast.sh
+++ shared/runlast.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ############################################################################
-# runlast.sh - (C)opyright 2019 OneCD [one.cd.only@gmail.com]
+# runlast.sh - (C)opyright 2019-2020 OneCD [one.cd.only@gmail.com]
 #
 # This script is part of the 'RunLast' package
 #
@@ -28,47 +28,94 @@ Init()
 
     THIS_QPKG_NAME=RunLast
     CONFIG_PATHFILE=/etc/config/qpkg.conf
-    LC_ALL=C
 
-    [[ ! -e $CONFIG_PATHFILE ]] && { echo "file not found [$CONFIG_PATHFILE]"; exit 1 ;}
+    if [[ ! -e $CONFIG_PATHFILE ]]; then
+        echo "file not found [$CONFIG_PATHFILE]"
+        exit 1
+    fi
 
-    local QPKG_PATH=$(getcfg $THIS_QPKG_NAME Install_Path -f "$CONFIG_PATHFILE")
-    REAL_LOG_PATHFILE=${QPKG_PATH}/${THIS_QPKG_NAME}.log
-    TEMP_LOG_PATHFILE=${REAL_LOG_PATHFILE}.tmp
-    GUI_LOG_PATHFILE=/home/httpd/${THIS_QPKG_NAME}.log
-    SCRIPT_STORE_PATH=${QPKG_PATH}/scripts
-    BUILD=$(getcfg $THIS_QPKG_NAME Build -f $CONFIG_PATHFILE)
+    readonly GETCFG_CMD=/sbin/getcfg
+    readonly RMCFG_CMD=/sbin/rmcfg
+    local -r QPKG_PATH=$($GETCFG_CMD $THIS_QPKG_NAME Install_Path -f "$CONFIG_PATHFILE")
+    readonly REAL_LOG_PATHFILE=$QPKG_PATH/$THIS_QPKG_NAME.log
+    readonly TEMP_LOG_PATHFILE=$REAL_LOG_PATHFILE.tmp
+    local -r GUI_LOG_PATHFILE=/home/httpd/$THIS_QPKG_NAME.log
+    readonly SYSV_STORE_PATH=$QPKG_PATH/init.d
+    readonly SCRIPT_STORE_PATH=$QPKG_PATH/scripts
+    readonly BUILD=$($GETCFG_CMD $THIS_QPKG_NAME Build -f $CONFIG_PATHFILE)
+    readonly LC_ALL=C
+
     echo "$THIS_QPKG_NAME ($BUILD)"
 
     [[ ! -e $REAL_LOG_PATHFILE ]] && touch "$REAL_LOG_PATHFILE"
     [[ -e $TEMP_LOG_PATHFILE ]] && rm -f "$TEMP_LOG_PATHFILE"
     [[ ! -L $GUI_LOG_PATHFILE ]] && ln -s "$REAL_LOG_PATHFILE" "$GUI_LOG_PATHFILE"
+    [[ ! -d $SYSV_STORE_PATH ]] && mkdir -p "$SYSV_STORE_PATH"
     [[ ! -d $SCRIPT_STORE_PATH ]] && mkdir -p "$SCRIPT_STORE_PATH"
+
+    }
+
+ProcessSysV()
+    {
+
+    # $1 = 'start' or 'stop'
+
+    [[ -z $1 ]] && return
+
+    local script_pathname=''
+
+    case $1 in
+        start)
+            # execute 'init.d' script names in-order
+            ls "$SYSV_STORE_PATH"/* 2>/dev/null | while read script_pathname; do
+                [[ -x $script_pathname ]] && RunAndLog "'$script_pathname' start"
+            done
+            ;;
+        stop)
+            # execute 'init.d' script names in reverse-order
+            ls -r "$SYSV_STORE_PATH"/* 2>/dev/null | while read script_pathname; do
+                [[ -x $script_pathname ]] && RunAndLog "'$script_pathname' stop"
+            done
+            ;;
+        *)
+            return 1
+            ;;
+    esac
 
     }
 
 ProcessScripts()
     {
 
-    [[ $exitcode -gt 0 ]] && return
+    local script_pathname=''
+
+    # read 'scripts' script names in order and execute
+    ls "$SCRIPT_STORE_PATH"/* 2>/dev/null | while read script_pathname; do
+        [[ -x $script_pathname ]] && RunAndLog "'$script_pathname'"
+    done
+
+    }
+
+RunAndLog()
+    {
+
+    # $1 = command to run
+
+    [[ -z $1 ]] && return 1
 
     echo >> "$TEMP_LOG_PATHFILE"
-    for i in ${SCRIPT_STORE_PATH}/*; do
-        if [[ -x $i ]]; then
-            local op="executing '$i':"
-            echo "[$(date)] $op" >> "$TEMP_LOG_PATHFILE"
-            echo "$op"
+    echo "[$(date)] executing $1:" | tee -a "$TEMP_LOG_PATHFILE"
 
-            {   # https://unix.stackexchange.com/a/430182/110015
-                stdout=$(eval $op 2> /dev/fd/3)
-                exitcode=$?
-                stderr=$(cat<&3)
-            } 3<<EOF
-            EOF
+    {   # https://unix.stackexchange.com/a/430182/110015
+        stdout=$(eval "$1" 2> /dev/fd/3)
+        returncode=$?
+        stderr=$(cat<&3)
+    } 3<<EOF
+EOF
 
-            echo -e "exitcode=[$exitcode]\nstdout=[$stdout]\nstderr=[$stderr]" | tee -a "$TEMP_LOG_PATHFILE"
-        fi
-    done
+    echo -e "returncode=[$returncode]\nstdout=[$stdout]\nstderr=[$stderr]" | tee -a "$TEMP_LOG_PATHFILE"
+
+    return 0
 
     }
 
@@ -78,9 +125,12 @@ SendToEnd()
     # sends $1 to the end of qpkg.conf
 
     local buffer=$(ShowDataBlock "$1")
-    [[ $? -gt 0 ]] && { echo "error - ${buffer}!"; return 2 ;}
+    if [[ $? -gt 0 ]]; then
+        echo "error - ${buffer}!"
+        return 2
+    fi
 
-    rmcfg "$1" -f "$CONFIG_PATHFILE"
+    $RMCFG_CMD "$1" -f "$CONFIG_PATHFILE"
     echo -e "$buffer" >> "$CONFIG_PATHFILE"
 
     }
@@ -91,14 +141,14 @@ ShowDataBlock()
     # returns the data block for the QPKG name specified as $1
 
     [[ -z $1 ]] && { echo "QPKG not specified"; return 1 ;}
-    ! (grep -q $1 $CONFIG_PATHFILE) && { echo "QPKG not found"; return 2 ;}
+    ! (grep -q "$1" $CONFIG_PATHFILE) && { echo "QPKG not found"; return 2 ;}
 
     sl=$(grep -n "^\[$1\]" "$CONFIG_PATHFILE" | cut -f1 -d':')
     ll=$(wc -l < "$CONFIG_PATHFILE" | tr -d ' ')
     bl=$(tail -n$((ll-sl)) < "$CONFIG_PATHFILE" | grep -n '^\[' | head -n1 | cut -f1 -d':')
     [[ ! -z $bl ]] && el=$((sl+bl-1)) || el=$ll
 
-    echo "$(sed -n "$sl,${el}p" "$CONFIG_PATHFILE")"
+    sed -n "$sl,${el}p" "$CONFIG_PATHFILE"
 
     }
 
@@ -111,7 +161,7 @@ TrimGUILog()
 
     if [[ $op_count -gt $max_ops ]]; then
         local last_op_line_num=$(echo "$op_lines" | head -n$((max_ops+1)) | tail -n1 | cut -f1 -d:)
-        head -n${last_op_line_num} "$REAL_LOG_PATHFILE" > "$TEMP_LOG_PATHFILE"
+        head -n"$last_op_line_num" "$REAL_LOG_PATHFILE" > "$TEMP_LOG_PATHFILE"
         mv "$TEMP_LOG_PATHFILE" "$REAL_LOG_PATHFILE"
     fi
 
@@ -127,7 +177,7 @@ RecordStart()
     local length=${#buffer}
     local temp=$(printf "%${length}s")
 
-    echo -e "${temp// /─}\n$THIS_QPKG_NAME ($BUILD)\n$buffer" >> "$TEMP_LOG_PATHFILE"
+    echo -e "${temp// /─}\n$THIS_QPKG_NAME ($BUILD)\n$buffer" > "$TEMP_LOG_PATHFILE"
 
     WriteQTSLog "$op" 0
     echo "$op"
@@ -146,6 +196,7 @@ RecordComplete()
 
     WriteQTSLog "$op" 0
     echo "$op"
+    CommitGUILog
 
     }
 
@@ -180,7 +231,7 @@ RecordError()
 CommitGUILog()
     {
 
-    echo -e "$(<$TEMP_LOG_PATHFILE)\n$(<$REAL_LOG_PATHFILE)" > "$REAL_LOG_PATHFILE"
+    echo -e "$(<"$TEMP_LOG_PATHFILE")\n$(<"$REAL_LOG_PATHFILE")" > "$REAL_LOG_PATHFILE"
 
     TrimGUILog
 
@@ -205,17 +256,13 @@ IsQPKGEnabled()
     # input:
     #   $1 = package name to check
     # output:
-    #   $package_is_enabled = true / false
     #   $? = 0 (true) or 1 (false)
-
-    package_is_enabled=false
 
     [[ -z $1 ]] && return 1
 
-    if [[ $(getcfg "$1" Enable -u -f "$CONFIG_PATHFILE") != 'TRUE' ]]; then
+    if [[ $($GETCFG_CMD "$1" Enable -u -f "$CONFIG_PATHFILE") != 'TRUE' ]]; then
         return 1
     else
-        package_is_enabled=true
         return 0
     fi
 
@@ -225,23 +272,24 @@ Init
 
 case "$1" in
     start)
+        # shellcheck disable=SC2154
         if [[ $package_status = INSTALLING ]]; then
             operation='installation'
             RecordStart "$operation"
         else
             operation='script processing'
             RecordStart "$operation"
+            ProcessSysV start
             ProcessScripts
         fi
         RecordComplete "$operation"
-        CommitGUILog
         ;;
     stop)
         if [[ $package_status != REMOVE ]]; then
             operation='package shuffle'
             RecordStart "$operation"
-            if (IsQPKGEnabled SortMyQPKGs); then
-                if [[ $(getcfg SortMyQPKGs Version -d 0 -f $CONFIG_PATHFILE) -ge 181217 ]]; then
+            if IsQPKGEnabled SortMyQPKGs; then
+                if [[ $($GETCFG_CMD SortMyQPKGs Version -d 0 -f $CONFIG_PATHFILE) -ge 181217 ]]; then
                     RecordWarning "SortMyQPKGs will reorder this package"
                 else
                     RecordError "your SortMyQPKGs version is incompatible with this package"
@@ -250,12 +298,15 @@ case "$1" in
                 SendToEnd $THIS_QPKG_NAME
             fi
             RecordComplete "$operation"
-            CommitGUILog
         fi
+        operation='script processing'
+        RecordStart "$operation"
+        ProcessSysV stop
+        RecordComplete "$operation"
         ;;
     *)
-        echo "use '$0 start' to begin processing scripts path"
-        echo "use '$0 stop' to ensure this package is run last during bootup"
+        echo "use '$0 start' to execute files in the 'init.d' path, then the 'scripts' path"
+        echo "use '$0 stop' to execute files in the 'init.d' path in reverse order"
         ;;
 esac
 
